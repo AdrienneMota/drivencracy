11d9326bf7dffe2eb5bd01f0516c0eaa26433225
AdrienneMota <adriennemota97@gmail.com>
2022-12-04 16:28:37 -0400

    fix: add pluggin fulldate

--- src/middlewares/pollValide.middleware.js
+++ src/middlewares/pollValide.middleware.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs"
 import customParseFormat from "dayjs/plugin/customParseFormat.js"
+dayjs.extend(customParseFormat)
 
 export function pollValidate(req, res, next){
     const poll = req.body    
